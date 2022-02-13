d027bd9865aa73b47080ff3ebc647a2ba5b41814
Sreehari Pulickamadhom Sreedhar <49713741+orectique@users.noreply.github.com>
2022-02-13 20:38:55 +0530

    update flow

--- app.py
+++ app.py
@@ -31,10 +31,22 @@ async def on_message(message):
 )
         return await message.channel.send(response.choices[0].text)
 
-    if message.content.startswith('Guys, meet Baljeet'):
+    elif message.content.startswith('Guys, meet Baljeet'):
         response = openai.Completion.create(
   engine="text-davinci-001",
   prompt = 'Introducing Baljeet, a sarcastic classmate of Sreehari',
+  temperature=0.5,
+  max_tokens=60,
+  top_p=0.3,
+  frequency_penalty=1,
+  presence_penalty=0.5
+)
+        return await message.channel.send(response.choices[0].text)
+
+    elif message.author.id == '589783387384840211':
+        response = openai.Completion.create(
+  engine="text-davinci-001",
+  prompt = message.content,
   temperature=0.5,
   max_tokens=60,
   top_p=0.3,
