49926836913e2ea4914812736aeb0cbe20dbaac7
Sreehari Pulickamadhom Sreedhar <49713741+orectique@users.noreply.github.com>
2022-02-13 18:28:14 +0530

    Baljeet

---
+++ app.py
@@ -0,0 +1,61 @@
+import discord
+import os
+import random
+
+import openai
+from dotenv import load_dotenv
+
+
+client = discord.Client()
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        if random.random() > 0.16:
+            return
+        response = openai.Completion.create(
+  engine="text-davinci-001",
+  prompt = message.content,
+  temperature=0.5,
+  max_tokens=60,
+  top_p=0.3,
+  frequency_penalty=1,
+  presence_penalty=0.5
+)
+        return await message.channel.send(response.choices[0].text)
+
+    if message.content.startswith('Guys, meet Baljeet'):
+        response = openai.Completion.create(
+  engine="text-davinci-001",
+  prompt = 'Introducing Baljeet, a sarcastic classmate of Sreehari',
+  temperature=0.5,
+  max_tokens=60,
+  top_p=0.3,
+  frequency_penalty=1,
+  presence_penalty=0.5
+)
+        return await message.channel.send(response.choices[0].text)
+
+    else:
+        if random.random() <= 0.5:
+            return
+        response = openai.Completion.create(
+  engine="text-davinci-001",
+  prompt = message.content,
+  temperature=0.5,
+  max_tokens=60,
+  top_p=0.3,
+  frequency_penalty=1,
+  presence_penalty=0.5
+)
+        return await message.channel.send(response.choices[0].text)
+
+TOKEN = os.getenv("TOKEN")
+client.run(TOKEN)
